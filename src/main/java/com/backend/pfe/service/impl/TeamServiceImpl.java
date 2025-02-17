661f1d017a899c337557cfc6a5088b6fe411b98e
Wassim Rahali <wassim.rahali@sofrecom.com>
2025-02-17 01:24:40 +0100

    went to fix some error in relations

--- src/main/java/com/backend/pfe/service/impl/TeamServiceImpl.java
+++ src/main/java/com/backend/pfe/service/impl/TeamServiceImpl.java
@@ -2,7 +2,9 @@
 package com.backend.pfe.service.impl;
 
 import com.backend.pfe.entites.Team;
+import com.backend.pfe.entites.User;
 import com.backend.pfe.repository.TeamRepository;
+import com.backend.pfe.repository.UserRepository;
 import com.backend.pfe.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +14,14 @@ import org.springframework.stereotype.Service;
 public class TeamServiceImpl implements TeamService {
 
     private final TeamRepository teamRepository;
+    private final UserRepository userRepository;
 
     @Override
     public Team createTeam(Team team) {
+        // Fetch the complete User object from the database
+        User manager = userRepository.findById(Long.valueOf(team.getManager().getId()))
+                .orElseThrow(() -> new RuntimeException("Manager not found"));
+        team.setManager(manager);
         return teamRepository.save(team);
     }
 }
